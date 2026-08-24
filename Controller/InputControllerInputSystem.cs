42be602abd4f4039ffbdc7b8dfca302c55b53839
DarkMorninG <Bustercall@gmx.net>
2026-08-24 14:31:20 +0200

    added option to disable/enable group inputs on start

--- Controller/InputControllerInputSystem.cs
+++ Controller/InputControllerInputSystem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using DBH.Attributes;
 using DBH.Base;
 using DBH.Input.api.Extending;
@@ -13,6 +14,22 @@ namespace DBH.Input.Controller {
     public class InputControllerInputSystem : DBHMono, IInputController {
         [Grab]
         private List<IIDisposableInputSystem> inputSystems;
+
+        [SerializeField]
+        private List<GroupStatus> groupStatuses;
+
+        public override void OnStart() {
+            foreach (var groupStatus in groupStatuses) {
+                InputSystem.actions.FindActionMap(groupStatus.Group)
+                    .actions.ForEach(action => {
+                        if (groupStatus.EnabledOnStart) {
+                            action.Enable();
+                        } else {
+                            action.Disable();
+                        }
+                    });
+            }
+        }
 
         private void OnDestroy() {
             inputSystems.ForEach(buttonInputSystem => buttonInputSystem.Deconstruct());
@@ -33,5 +50,31 @@ namespace DBH.Input.Controller {
         public void AddButton(DirectionKeys keys) {
             throw new NotImplementedException();
         }
+
+        private void OnValidate() {
+            foreach (var actionsActionMap in InputSystem.actions.actionMaps) {
+                if (groupStatuses.All(status => status.Group != actionsActionMap.name)) {
+                    groupStatuses.Add(new GroupStatus(actionsActionMap.name, true));
+                }
+            }
+        }
+    }
+
+    [Serializable]
+    public class GroupStatus {
+        [SerializeField]
+        private string group;
+
+        [SerializeField]
+        private bool enabledOnStart;
+
+        public GroupStatus(string group, bool enabledOnStart) {
+            this.group = group;
+            this.enabledOnStart = enabledOnStart;
+        }
+
+        public string Group => group;
+
+        public bool EnabledOnStart => enabledOnStart;
     }
 }
